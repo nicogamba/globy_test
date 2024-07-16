475b6348ec90764ea1f96b768e11cd6a3700cd37
Nicolas Gamba <nicolasgamba.lp@gmail.com>
2024-07-15 23:12:17 -0300

    Modificar tiempo de caché para configurar por variable de entorno

--- src/Service/StarWarsApiService.php
+++ src/Service/StarWarsApiService.php
@@ -4,6 +4,7 @@ namespace App\Service;
 
 use GuzzleHttp\Client;
 use Psr\Cache\CacheItemPoolInterface;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class StarWarsApiService
 {
@@ -11,23 +12,26 @@ class StarWarsApiService
     private const CACHE_KEY = 'star_wars_people_page_';
     private $client;
     private $cache;
+    private $params;
 
-    public function __construct(Client $client, CacheItemPoolInterface $cache)
+    public function __construct(Client $client, CacheItemPoolInterface $cache, ParameterBagInterface $params)
     {
         $this->client = $client;
         $this->cache = $cache;
+        $this->params = $params;
     }
 
     public function fetchPeople(int $page): array
     {
         $cacheKey = self::CACHE_KEY . $page;
         $cacheItem = $this->cache->getItem($cacheKey);
+        $cacheTime = intval($this->params->get('cache_expiry'));
 
         if (!$cacheItem->isHit()) {
             $response = $this->client->get(self::SWAPI_URL . "?page={$page}");
             $data = json_decode($response->getBody(), true);
             $cacheItem->set($data);
-            $cacheItem->expiresAfter(60);
+            $cacheItem->expiresAfter($cacheTime);
             $this->cache->save($cacheItem);
         } else {
             $data = $cacheItem->get();
